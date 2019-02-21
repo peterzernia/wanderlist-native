beb5199666125cfa5fbe385448305a6c08364b14
Peter Zernia <peter176@gmail.com>
2019-02-21 15:10:58 +0100

    removed create-react-app-native boiler plate

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { View } from 'react-native';
 
 export default class FeedScreen extends React.Component {
   static navigationOptions = {
@@ -9,19 +8,8 @@ export default class FeedScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View></View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { View } from 'react-native';
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Profile',
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View></View>
+    );
   }
 }
