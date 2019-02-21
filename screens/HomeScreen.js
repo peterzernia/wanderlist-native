9096210c793e0017d68f984c8271d0a5e77183a7
Peter Zernia <peter176@gmail.com>
2019-02-21 16:04:29 +0100

    added header image to homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, Dimensions, StyleSheet} from 'react-native';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -8,7 +8,19 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View></View>
+      <View>
+        <Image 
+          style={styles.image}
+          source={{uri: 'https://s3.eu-west-3.amazonaws.com/countries-app/media/header.jpeg'}}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width*.75
+  }
+})
