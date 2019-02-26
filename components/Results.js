3de71c637f9bc25f0412215494104d4eb6414911
Peter Zernia <peter176@gmail.com>
2019-02-26 11:55:17 +0100

    results component

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, StyleSheet, View } from 'react-native'
 
 export default class Results extends Component {
   render() {
     return (
-      <View>
-        <Text>{this.props.country.name}</Text>
+      <View style={styles.card}>
+        <Text style={styles.header}>
+          {this.props.country.name}
+        </Text>
+        <Image 
+          style={styles.image}
+          source={{uri: this.props.country.flag}}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 5, 
+    borderWidth: .1,
+    width: '95%',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 23,
+    padding: 15,
+  },
+  image: {
+    width: '100%',
+    height: 400
+  }
+})
