ea992658222e55d00ed25b6a11e293ea5b87b6f2
Peter Zernia <peter176@gmail.com>
2019-03-04 21:13:54 +0100

    display flag on results component

--- components/Results.js
+++ components/Results.js
@@ -1,48 +1,73 @@
-import React from 'react';
-import { ActivityIndicator, Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Results(props) {
-  const { country, user, updatingUser, handleUpdate, pendingCountry } = props;
+export default class Results extends Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 0,
+    }
+  }
 
-  // Map the userCountries into an array of just the countries' names.
-  const userCountries = user.countries.map(country => country.name);
+  componentDidMount() {
+    var { alpha2code } = this.props.country;
+    alpha2code = alpha2code.toLowerCase();
 
+    Image.getSize(`https://raw.githubusercontent.com/peterzernia/flags/master/${alpha2code}.png`, (width, height) => {
+      this.setState({  
+        height: Dimensions.get('window').width*.95*height/width 
+      });
+    });
+  }
 
-  return (
-    <View style={styles.card}>
-      <View style={styles.header}>
-        {/* This empty View is for offsetting the button on the opposite side of the header text */}
-        <View style={styles.buttonContainer}></View>
-        <Text style={styles.headerText}>
-          {country.name}
-        </Text>
-        <TouchableOpacity 
-          style={styles.buttonContainer}
-          onPress={() => handleUpdate(country)}
-        >
-        {
-          // Render Remove button when country is included in users list, but is not the pending country.
-          (userCountries.includes(country.name) && (!updatingUser || (updatingUser && pendingCountry.name !== country.name)))
-          && <Icon name='remove-circle' size={25} />
-        }
-        {
-          // Render Add button when country is not included in users list, but is not the pending country.
-          (!userCountries.includes(country.name) && (!updatingUser || (updatingUser && pendingCountry.name !== country.name)))
-          && <Icon name='add-circle' size={25} />
-        }
-        {
-          // If the updatingUser, and the pendingCountry matches the country shown, render loader
-          (updatingUser && pendingCountry.name === country.name)
-          && <ActivityIndicator size="small" color="black" />
-        }
-        </TouchableOpacity>
+  render() {
+    const { country, user, updatingUser, handleUpdate, pendingCountry } = this.props;
+
+    // Map the userCountries into an array of just the countries' names.
+    const userCountries = user.countries.map(country => country.name);
+    var { alpha2code } = country;
+    alpha2code = alpha2code.toLowerCase();
+
+    return (
+      <View style={styles.card}>
+        <View style={styles.header}>
+          {/* This empty View is for offsetting the button on the opposite side of the header text */}
+          <View style={styles.buttonContainer}></View>
+          <Text style={styles.headerText}>
+            {country.name}
+          </Text>
+          <TouchableOpacity 
+            style={styles.buttonContainer}
+            onPress={() => handleUpdate(country)}
+          >
+          {
+            // Render Remove button when country is included in users list, but is not the pending country.
+            (userCountries.includes(country.name) && (!updatingUser || (updatingUser && pendingCountry.name !== country.name)))
+            && <Icon name='remove-circle' size={25} />
+          }
+          {
+            // Render Add button when country is not included in users list, but is not the pending country.
+            (!userCountries.includes(country.name) && (!updatingUser || (updatingUser && pendingCountry.name !== country.name)))
+            && <Icon name='add-circle' size={25} />
+          }
+          {
+            // If the updatingUser, and the pendingCountry matches the country shown, render loader
+            (updatingUser && pendingCountry.name === country.name)
+            && <ActivityIndicator size="small" color="black" />
+          }
+          </TouchableOpacity>
+        </View>
+        <View style={{width: Dimensions.get('window').width*.95, height: this.state.height}}>
+          <Image
+            style={{width: Dimensions.get('window').width*.95, height: this.state.height, resizeMode: 'cover'}}
+            source={{uri: `https://raw.githubusercontent.com/peterzernia/flags/master/${alpha2code}.png`}}
+          />
+        </View>
       </View>
-      <View style={styles.imageContainer}>
-      </View>
-    </View>
-  )
+    )
+  } 
 }
 
 Results.propTypes = {
@@ -59,7 +84,6 @@ const styles = StyleSheet.create({
     borderRadius: 5, 
     borderWidth: .1,
     width: '95%',
-    alignItems: 'center',
     marginBottom: 10
   },
   header: {
@@ -83,8 +107,4 @@ const styles = StyleSheet.create({
     width: 25,
     height: 25,
   },
-  imageContainer: {
-    width: Dimensions.get('window').width*.95,
-    height: Dimensions.get('window').width*.95*2/3
-  }
 });
