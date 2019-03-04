81371a54005db4ab1e1998b83fffeb4a758bd0a7
Peter Zernia <peter176@gmail.com>
2019-03-04 12:50:09 +0100

    made Trip Report footer into reusable component

--- components/TripReportCard.js
+++ components/TripReportCard.js
@@ -1,60 +1,35 @@
-import React, { PureComponent } from 'react'
-import { Text, StyleSheet, TouchableOpacity, View } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity, View } from 'react-native';
+import TripReportFooter from './TripReportFooter';
 
-export default class TripReportCard extends PureComponent {
-  render() {
-    const { tripReport, user, navigation, handlePress, onShare} = this.props;
+// TripReportCard is shown on FeedScreen in a FlatList.
+export default function TripReportCard(props) {
+  const { tripReport, navigation } = props;
 
-    const listCountries = tripReport.countries.map(country => {
-      <Text>{country.name}</Text>
-    });
-    return (
-      <TouchableOpacity 
-        style={styles.card}
-        // Pass tripReport prop into navigation to Trip Report Screen.
-        onPress={() => navigation.navigate('TripReport', {tripReport: tripReport})}
-      >
-        <View style={styles.header}>
-          <Text style={styles.title}>{tripReport.title} </Text>
-          <Text style={styles.author}>{tripReport.author.username}</Text>
-        </View>
-        <View style={styles.body}>
-          <Text numberOfLines={3}>
-            {tripReport.content}
-          </Text>
-        </View>
-        <View style={styles.countriesSection}>
-          {listCountries}
-        </View>
-        <View style={styles.footer}>
-          <View style={styles.favoriteButtonContainer}>
-            {/* If the trip report favorite count is not 0, display the count. */}
-            {
-             (!!tripReport.favoriters.length) && 
-             <Text style={styles.favoriteCount}>{tripReport.favoriters.length}</Text>
-            }
-            <TouchableOpacity 
-              style={styles.favoriteButton}
-              onPress={() => handlePress(tripReport.id)}
-            >
-            {
-              tripReport.favoriters.includes(user.pk)
-              ? <Icon name='favorite' size={25} />
-              : <Icon name='favorite-border' size={25} />
-            }
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity 
-            style={styles.shareButton}
-            onPress={() => onShare(tripReport.slug)}
-          >
-            <Icon name='share' size={25} />
-          </TouchableOpacity>
-        </View>
-      </TouchableOpacity>
-    )
-  }
+  const listCountries = tripReport.countries.map(country => {
+    <Text>{country.name}</Text>
+  });
+  return (
+    <TouchableOpacity 
+      style={styles.card}
+      // Pass tripReport prop into navigation to Trip Report Screen.
+      onPress={() => navigation.navigate('TripReport', {tripReport: tripReport})}
+    >
+      <View style={styles.header}>
+        <Text style={styles.title}>{tripReport.title} </Text>
+        <Text style={styles.author}>{tripReport.author.username}</Text>
+      </View>
+      <View style={styles.body}>
+        <Text numberOfLines={3}>
+          {tripReport.content}
+        </Text>
+      </View>
+      <View style={styles.countriesSection}>
+        {listCountries}
+      </View>
+      <TripReportFooter {...props} />
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -81,23 +56,5 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   countriesSection: {
-
   },
-  footer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 5,
-  },
-  favoriteButtonContainer: {
-    flexDirection: 'row',
-  },
-  favoriteCount: {
-    fontSize: 18,
-    marginRight: 2
-  },
-  favoriteButton: {
-  },
-  shareButton: {
-  }
-})
+});
---
+++ components/TripReportFooter.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+// Reusable TripReportFooter Component
+export default function TripReportFooter(props) {
+  const { tripReport, user, handlePress, onShare} = props;
+  
+  return (
+    <View style={styles.footer}>
+      <View style={styles.favoriteButtonContainer}>
+        {/* If the trip report favorite count is not 0, display the count. */}
+        {
+        (!!tripReport.favoriters.length) &&
+        <Text style={styles.favoriteCount}>{tripReport.favoriters.length}</Text>
+        }
+        <TouchableOpacity 
+          style={styles.favoriteButton}
+          onPress={() => handlePress(tripReport.id)}
+        >
+        {/* Display border icon if Trip Report is not like. */}
+        {
+          tripReport.favoriters.includes(user.pk)
+          ? <Icon name='favorite' size={25} />
+          : <Icon name='favorite-border' size={25} />
+        }
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity 
+        style={styles.shareButton}
+        onPress={() => onShare(tripReport.slug)}
+      >
+        <Icon name='share' size={25} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  footer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 5,
+  },
+  favoriteButtonContainer: {
+    flexDirection: 'row',
+  },
+  favoriteCount: {
+    fontSize: 18,
+    marginRight: 2
+  },
+  favoriteButton: {
+  },
+  shareButton: {
+  }
+})
