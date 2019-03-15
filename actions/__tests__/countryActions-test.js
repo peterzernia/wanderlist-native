6000dc222101ef86d0e38f6becfdca33e97b84bd
Peter Zernia <peter176@gmail.com>
2019-03-15 17:25:02 +0100

    country action creator tests

---
+++ actions/__tests__/countryActions-test.js
@@ -0,0 +1,24 @@
+import * as countryActions from "../countryActions";
+
+const countries = [{ name: "Test" }];
+
+// Action creator tests.
+describe("Country Action Creators", () => {
+  it("should create a FETCH_COUNTRIES_PENDING action", () => {
+    const expectedAction = { type: "FETCH_COUNTRIES_PENDING" };
+    expect(countryActions.fetchCountriesPending()).toEqual(expectedAction);
+  });
+  it("should create a FETCH_COUNTRIES_FULFILLED action", () => {
+    const expectedAction = {
+      type: "FETCH_COUNTRIES_FULFILLED",
+      countries
+    };
+    expect(countryActions.fetchCountriesFulfilled(countries)).toEqual(
+      expectedAction
+    );
+  });
+  it("should create a FETCH_COUNTRIES_REJECTED action", () => {
+    const expectedAction = { type: "FETCH_COUNTRIES_REJECTED" };
+    expect(countryActions.fetchCountriesRejected()).toEqual(expectedAction);
+  });
+});
