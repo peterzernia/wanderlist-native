d1aeed6878c9532f558858fef2466dec575b46cc
Peter Zernia <peter176@gmail.com>
2019-03-17 15:26:19 +0100

    TripReportHeader tests

---
+++ components/__tests__/TripReportHeader-test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import TripReportHeader from "../TripReportHeader";
+import { Alert } from "react-native";
+
+describe("<TripReportHeader />", () => {
+  const author = {
+    username: "Test",
+    pk: 1,
+    home: { name: "test", flag: "https://test.com" }
+  };
+  const tripReport = {
+    title: "Test",
+    countries: [{ name: "Test", id: 1 }],
+    favoriters: [],
+    author
+  };
+  const props = { tripReport };
+
+  // Setup wrapper
+  beforeEach(() => {
+    wrapper = shallow(<TripReportHeader {...props} />);
+  });
+
+  it("alerts", () => {
+    spy = jest.spyOn(Alert, "alert");
+    wrapper
+      .find("TouchableOpacity")
+      .at(0)
+      .simulate("press");
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
