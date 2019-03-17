5f03f4229201c77ece48ee7c9a05a8a9057340ee
Peter Zernia <peter176@gmail.com>
2019-03-17 16:13:49 +0100

    CountryMap tests

---
+++ components/__tests__/CountryMap-test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CountryMap from "../CountryMap";
+
+describe("<CountryMap />", () => {
+  let wrapper;
+  const country = { latlng: [90, 90] };
+  let props = { country };
+
+  // Setup wrapper
+  beforeEach(() => {
+    wrapper = shallow(<CountryMap {...props} />);
+  });
+
+  it("initial region is Country coordinates", () => {
+    expect(wrapper.state("region")).toEqual({
+      latitude: country.latlng[0],
+      longitude: country.latlng[1],
+      latitudeDelta: 100,
+      longitudeDelta: 50
+    });
+  });
+});
