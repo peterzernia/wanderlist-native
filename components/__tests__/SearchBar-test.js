df2ecb3bf5fe37fb2ade7c5f92076ee45217fdd4
Peter Zernia <peter176@gmail.com>
2019-03-18 09:00:57 +0100

    more SearchBar tests

--- components/__tests__/SearchBar-test.js
+++ components/__tests__/SearchBar-test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import SearchBar from "../SearchBar";
 import { ActivityIndicator } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { TouchableOpacity } from "react-native";
 
 describe("<SearchBar />", () => {
   let wrapper;
@@ -23,5 +25,36 @@ describe("<SearchBar />", () => {
     const fetchingCountries = true;
     wrapper.setProps({ ...props, fetchingCountries });
     expect(wrapper.find(ActivityIndicator).length).toEqual(1);
+  });
+
+  it("autocomplete changes query onChangeText", () => {
+    wrapper.find("Autocomplete").simulate("changeText", "test");
+    expect(wrapper.state("query")).toEqual("test");
+    expect(wrapper.state("hide")).toEqual(false);
+  });
+
+  it("TouchableOpacity removes query & query renders Icon", () => {
+    wrapper.setState({ query: "test" });
+    expect(wrapper.state("query")).toEqual("test");
+    expect(wrapper.find(Icon).length).toEqual(1);
+    wrapper
+      .find("TouchableOpacity")
+      .at(0)
+      .simulate("press");
+    expect(wrapper.state("query")).toEqual("");
+    expect(wrapper.find(Icon).length).toEqual(0);
+  });
+
+  // it("query renders buttons for each country that matches query", () => {
+  //   wrapper.setState({ query: "united" });
+  //   expect(wrapper.find("Text").length).toEqual(5);
+  // });
+
+  it("handles Search", () => {
+    wrapper
+      .find("TouchableOpacity")
+      .at(1)
+      .simulate("press");
+    expect(handleSearch).toHaveBeenCalledTimes(1);
   });
 });
