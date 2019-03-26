091d9dd4b5cadc1959d9df284fdd2c2a4357d3ed
Peter Zernia <peter176@gmail.com>
2019-03-26 17:42:41 +0100

    globalActions test

---
+++ actions/__tests__/globalActions-test.js
@@ -0,0 +1,12 @@
+import * as globalActions from "../globalActions";
+
+describe("globalAction Creator", () => {
+  it("should create a SET_STATE action", () => {
+    const globalState = { test: "Test" };
+    const expectedAction = {
+      type: "SET_STATE",
+      globalState
+    };
+    expect(globalActions.setState(globalState)).toEqual(expectedAction);
+  });
+});
