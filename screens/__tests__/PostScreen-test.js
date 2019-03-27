021f722b4c0c78d5f7fee0a026bf67dfc9f1a7af
Peter Zernia <peter176@gmail.com>
2019-03-27 13:26:17 +0100

    PostScreen tests

--- screens/__tests__/PostScreen-test.js
+++ screens/__tests__/PostScreen-test.js
@@ -1,21 +1,51 @@
 import React from "react";
 import { shallow } from "enzyme";
-import PostScreen from "../PostScreen";
+import { PostScreen } from "../PostScreen";
+import PostTitleHeader from "../../components/PostTitleHeader";
 
 describe("<PostScreen />", () => {
   let wrapper;
-  let tripReport = null;
+  let user = { username: "TestUser" };
+  let globalState = {};
+  const setState = jest.fn();
+  let tripReport = { title: "Test", content: "Test" };
   const params = { tripReport };
   const state = { params };
   const navigation = { state };
-  let props = { navigation };
+  let props = { user, globalState, setState, navigation };
 
   // Setup wrapper
   beforeEach(() => {
     wrapper = shallow(<PostScreen {...props} />);
   });
 
-  it("renders", () => {
-    expect(wrapper.find("View").length).toEqual(0);
+  /**
+   * Navigation header shows New Trip Report when the navProps doesn't contain
+   * params variable and Edit Trip Report when it does.
+   */
+  it("navigationOptions", () => {
+    let navigationOptions = PostScreen.navigationOptions(props);
+    expect(navigationOptions).toEqual({
+      title: "Edit Trip Report",
+      headerTitle: <PostTitleHeader {...params} navigation={navigation} />
+    });
+
+    let navProp = { navigation: { state: {} } };
+    navigationOptions = PostScreen.navigationOptions({ ...navProp });
+    expect(navigationOptions).toEqual({
+      title: "New Trip Report",
+      headerTitle: <PostTitleHeader navigation={{ state: {} }} />
+    });
+  });
+
+  /**
+   * When a tripReport is passed as a parameter, <PostForm/> has the prop
+   * tripReport=tripReport. When there is no tripReport, the prop tripReport=null
+   */
+  it("post form tripReport prop has tripReport or is null", () => {
+    expect(wrapper.find("PostForm").prop("tripReport")).toEqual(tripReport);
+
+    wrapper.setProps({ ...props, navigation: { state: {} } });
+    expect(wrapper.find("PostForm").prop("tripReport")).toEqual(undefined);
   });
 });
