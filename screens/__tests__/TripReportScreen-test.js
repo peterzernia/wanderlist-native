430cdaa8dcf2fb0392fa043b9ed338bbbb2b02aa
Peter Zernia <peter176@gmail.com>
2019-03-27 14:51:25 +0100

    more TripReportScreen tests

--- screens/__tests__/TripReportScreen-test.js
+++ screens/__tests__/TripReportScreen-test.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
-import TripReportScreen from "../TripReportScreen";
+import { TripReportScreen } from "../TripReportScreen";
+import TripReportTitleHeader from "../../components/TripReportTitleHeader";
 
 describe("<TripReportScreen />", () => {
   let wrapper;
   const deleteTripReport = jest.fn();
-  const tripReports = [{ title: "Test", id: 1 }];
-  const tripReport = tripReports[1];
+  const tripReports = { results: [{ title: "Test", id: 1 }] };
+  const tripReport = { title: "Test", id: 1 };
+  const setParams = jest.fn();
+  const getParam = jest.fn();
   const params = { tripReport };
   const state = { params };
-  const navigation = { state };
+  const navigation = { state, setParams, getParam };
   let props = { deleteTripReport, tripReports, navigation };
 
   // Setup wrapper
@@ -17,7 +20,20 @@ describe("<TripReportScreen />", () => {
     wrapper = shallow(<TripReportScreen {...props} />);
   });
 
-  it("renders", () => {
-    expect(wrapper.find("View").length).toEqual(0);
+  it("navigationOptions", () => {
+    const navigationOptions = TripReportScreen.navigationOptions(props);
+    expect(navigationOptions).toEqual({
+      headerTitle: (
+        <TripReportTitleHeader
+          {...params}
+          handleDelete={navigation.getParam("handleDelete")}
+        />
+      )
+    });
+  });
+
+  it("handles Delete", async () => {
+    await wrapper.instance().handleDelete();
+    expect(deleteTripReport).toHaveBeenCalledTimes(1);
   });
 });
