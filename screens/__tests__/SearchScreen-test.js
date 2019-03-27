e30df24e1f6ca3408573bc60f385fb4b71b291d3
Peter Zernia <peter176@gmail.com>
2019-03-27 14:34:59 +0100

    More SearchScreen tests

--- screens/__tests__/SearchScreen-test.js
+++ screens/__tests__/SearchScreen-test.js
@@ -1,20 +1,50 @@
 import React from "react";
 import { shallow } from "enzyme";
-import SearchScreen from "../SearchScreen";
+import { SearchScreen } from "../SearchScreen";
 
 describe("<SearchScreen />", () => {
   let wrapper;
   const fetchCountries = jest.fn();
-  const user = { username: "Test" };
-  let updateUser = jest.fn();
-  let props = { fetchCountries, user, updateUser };
+  let fetchingCountries = false;
+  const user = {
+    username: "Test",
+    countries: [],
+    home: { id: 1, name: "TestHome" },
+    biography: "Test"
+  };
+  const updateUser = jest.fn();
+  let updatingUser = false;
+  let countries = [];
+  let props = {
+    fetchCountries,
+    fetchingCountries,
+    user,
+    updateUser,
+    updatingUser,
+    countries
+  };
 
   // Setup wrapper
   beforeEach(() => {
     wrapper = shallow(<SearchScreen {...props} />);
   });
 
-  it("renders", () => {
-    expect(wrapper.find("View").length).toEqual(0);
+  it("handles Search", () => {
+    wrapper.instance().handleSearch();
+    expect(fetchCountries).toHaveBeenCalledTimes(1);
+  });
+
+  /**
+   * Must await handleUpdate because it calls AsyncStorage.getItem()
+   * which returns a promise.
+   */
+  it("handles Update", async () => {
+    await wrapper.instance().handleUpdate({ name: "TestCountry", id: 2 });
+    expect(updateUser).toHaveBeenCalledTimes(1);
+  });
+
+  it("renders header", () => {
+    let header = wrapper.instance().renderHeader();
+    expect(header).not.toEqual(null);
   });
 });
