658b254971191c530c7a08ab7fe455e66bc880fa
Peter Zernia <peter176@gmail.com>
2019-03-27 12:07:25 +0100

    ForgotPasswordScreen tests

--- screens/__tests__/ForgotPasswordScreen-test.js
+++ screens/__tests__/ForgotPasswordScreen-test.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { shallow } from "enzyme";
-import ForgotPasswordScreen from "../ForgotPasswordScreen";
+import { ForgotPasswordScreen } from "../ForgotPasswordScreen";
 
 describe("<ForgotPasswordScreen />", () => {
   let wrapper;
   const requestPasswordReset = jest.fn();
   const authenticated = false;
+  const authenticating = false;
   const navigate = jest.fn();
   const navigation = { navigate };
-  let props = { requestPasswordReset, navigation, authenticated };
+  let props = {
+    requestPasswordReset,
+    navigation,
+    authenticated,
+    authenticating
+  };
 
   // Setup wrapper
   beforeEach(() => {
     wrapper = shallow(<ForgotPasswordScreen {...props} />);
   });
 
-  it("renders", () => {
-    expect(wrapper.find("View").length).toEqual(0);
+  it("component Mounts & navigates", () => {
+    wrapper.instance().componentDidUpdate();
+    expect(navigate).toHaveBeenCalledTimes(0);
+
+    wrapper.setProps({ ...props, authenticated: true });
+    expect(navigate).toHaveBeenCalledTimes(1);
+    wrapper.instance().componentDidUpdate();
+    expect(navigate).toHaveBeenCalledTimes(2);
+  });
+
+  it("handles Submit", () => {
+    wrapper.instance().handleSubmit();
+    expect(requestPasswordReset).toHaveBeenCalledTimes(1);
   });
 });
