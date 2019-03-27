1557f407d0ca6c29e8d457e49d738b12484ed9ce
Peter Zernia <peter176@gmail.com>
2019-03-27 15:13:08 +0100

    tested different paths of adding/removing country to user's map on SearchScreen

--- screens/__tests__/SearchScreen-test.js
+++ screens/__tests__/SearchScreen-test.js
@@ -7,10 +7,11 @@ describe("<SearchScreen />", () => {
   const fetchCountries = jest.fn();
   let fetchingCountries = false;
   const user = {
-    username: "Test",
+    username: "TestUser",
+    email: "TestEmail",
     countries: [],
     home: { id: 1, name: "TestHome" },
-    biography: "Test"
+    biography: "TestBiography"
   };
   const updateUser = jest.fn();
   let updatingUser = false;
@@ -39,8 +40,61 @@ describe("<SearchScreen />", () => {
    * which returns a promise.
    */
   it("handles Update", async () => {
+    /**
+     * First time, handleUpdate is called when user has no countries
+     * in their list, adding the country to their list.
+     */
     await wrapper.instance().handleUpdate({ name: "TestCountry", id: 2 });
     expect(updateUser).toHaveBeenCalledTimes(1);
+    expect(updateUser).toHaveBeenLastCalledWith(
+      null, // token
+      "TestUser", // username
+      "TestEmail", // email
+      [2], // user's countries' list ids, contains TestCountry
+      1, // user's home country id
+      "TestBiography", // user's biography
+      "TestCountry has been added to your map." // success message
+    );
+
+    /**
+     * Second time, handleUpdate is called when user has TestCountry in
+     * their list, so it is removed.
+     */
+    wrapper.setProps({
+      ...props,
+      user: { ...user, countries: [{ name: "TestCountry", id: 2 }] }
+    });
+    await wrapper.instance().handleUpdate({ name: "TestCountry", id: 2 });
+    expect(updateUser).toHaveBeenCalledTimes(2);
+    expect(updateUser).toHaveBeenLastCalledWith(
+      null, // token
+      "TestUser", // username
+      "TestEmail", // email
+      [], // user's countries' list ids, does not contait TestCountry
+      1, // user's home country id
+      "TestBiography", // user's biography
+      "TestCountry has been removed to your map." // success message
+    );
+
+    /**
+     * Third time, handleUpdate is called when user has a country other
+     * than TestCountry in their list, so TestCountry is added.
+     */
+    wrapper.setProps({
+      ...props,
+      user: { ...user, countries: [{ name: "TestCountry2", id: 3 }] }
+    });
+    await wrapper.instance().handleUpdate({ name: "TestCountry", id: 2 });
+    expect(updateUser).toHaveBeenCalledTimes(3);
+    expect(updateUser).toHaveBeenLastCalledWith(
+      null, // token
+      "TestUser", // username
+      "TestEmail", // email
+      [3, 2], // user's countries' list ids, does not contait TestCountry
+      1, // user's home country id
+      "TestBiography", // user's biography
+      "TestCountry has been added to your map." // success message
+    );
   });
 
   it("renders header", () => {
