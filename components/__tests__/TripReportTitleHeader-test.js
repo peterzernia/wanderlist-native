607a3d05bc8cf3fa49c60a95ef77419a2aa93ee0
Peter Zernia <peter176@gmail.com>
2019-03-27 16:19:28 +0100

    TripReportTitleHeader tests

--- components/__tests__/TripReportTitleHeader-test.js
+++ components/__tests__/TripReportTitleHeader-test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import TripReportTitleHeader from "../TripReportTitleHeader";
+import { Alert, Modal } from "react-native";
 
 describe("<TripReportTitleHeader />", () => {
   let wrapper;
@@ -20,7 +21,60 @@ describe("<TripReportTitleHeader />", () => {
     wrapper = shallow(<TripReportTitleHeader {...props} />);
   });
 
-  it("renders", () => {
+  /**
+   * Initially there is a component since the authenticated user
+   * is the author, but when the author is changed, it renders null
+   */
+  it("renders conditionally", () => {
     expect(wrapper.find("TouchableOpacity").length).toEqual(4);
+    wrapper.setProps({
+      ...props,
+      tripReport: {
+        ...tripReport,
+        author: { pk: 2, home: { flag: "https://test.com/" } }
+      }
+    });
+    expect(wrapper.html()).toBeNull();
+  });
+
+  it("opens & closes modal", () => {
+    // First TouchableOpacity opens Modal.
+    expect(wrapper.state("modalVisible")).toEqual(false);
+    wrapper
+      .find("TouchableOpacity")
+      .at(0)
+      .simulate("press");
+    expect(wrapper.state("modalVisible")).toEqual(true);
+
+    // onRequestClose closes Modal.
+    wrapper.find(Modal).simulate("requestClose");
+    expect(wrapper.state("modalVisible")).toEqual(false);
+
+    // Second TouchableOpacity closes Modal onPressOut.
+    wrapper.setState({ modalVisible: true });
+    wrapper
+      .find("TouchableOpacity")
+      .at(1)
+      .simulate("pressOut");
+    expect(wrapper.state("modalVisible")).toEqual(false);
+  });
+
+  it("third TouchableOpacity navigates & closes modal", () => {
+    wrapper.setState({ modalVisible: true });
+    wrapper
+      .find("TouchableOpacity")
+      .at(2)
+      .simulate("press");
+    expect(navigate).toHaveBeenCalledTimes(1);
+    expect(wrapper.state("modalVisible")).toEqual(false);
+  });
+
+  it("fourth TouchableOpacity alerts", () => {
+    spy = jest.spyOn(Alert, "alert");
+    wrapper
+      .find("TouchableOpacity")
+      .at(3)
+      .simulate("press");
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
