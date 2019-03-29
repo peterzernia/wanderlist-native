a1c1ffe3c211fe574f412ca82c15e21ed3565c00
Peter Zernia <peter176@gmail.com>
2019-03-29 11:55:02 +0100

    authActions authCheckState async test

--- actions/__tests__/authActions-test.js
+++ actions/__tests__/authActions-test.js
@@ -1,4 +1,43 @@
 import * as authActions from "../authActions";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe("async action creators", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      token: null,
+      authenticating: false,
+      authenticated: false
+    });
+  });
+
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  /**
+   * If token is null, AUTH_LOGOUT is dispatched, if there is a
+   * token, AUTH_SUCCESS is dispatched.
+   */
+  it("should check auth state", () => {
+    let token = null;
+    store.dispatch(authActions.authCheckState(token));
+    let actions = store.getActions();
+    expect(actions[0]).toEqual(authActions.authLogout());
+
+    store.clearActions();
+
+    token = "testtoken";
+    store.dispatch(authActions.authCheckState(token));
+    actions = store.getActions();
+    expect(actions[0]).toEqual(authActions.authSuccess(token));
+  });
+});
 
 describe("Auth Action Creators", () => {
   it("should create a AUTH_START action", () => {
