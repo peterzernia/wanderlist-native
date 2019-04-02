8805f9a0805f16e51b985e1386d9ee40fe0caa82
Peter Zernia <peter176@gmail.com>
2019-04-02 11:51:39 +0200

    handleFavorite tests

--- constants/__tests__/Functions-test.js
+++ constants/__tests__/Functions-test.js
@@ -1,10 +1,19 @@
 import * as functions from "../Functions";
 import { Share } from "react-native";
+import store from "../../reducers/index";
 
 describe("handleShare tests", () => {
   it("shares", async () => {
     spy = jest.spyOn(Share, "share");
     await functions.handleShare("ThyN5Mk3");
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("handleFavorite tests", () => {
+  it("dispatches", async () => {
+    spy = jest.spyOn(store, "dispatch");
+    await functions.handleFavorite(1);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 });
